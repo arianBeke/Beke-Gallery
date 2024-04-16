0cce0f2900c9bebafb6fe5539c02805ccaa0e324
Arian Beke <131852639+arianBeke@users.noreply.github.com>
2024-04-16 13:44:00 +0200

    fix

--- app/pages/support/page.jsx
+++ app/pages/support/page.jsx
@@ -15,7 +15,6 @@ const ContactPage = () => {
     const message = form.current.elements.message.value;
 
     if (!name || !email || !message) {
-      // Notify the user that the form is incomplete
       toast.error('Please fill in all the fields.');
       return;
     }
@@ -44,7 +43,7 @@ const ContactPage = () => {
         <form ref={form} onSubmit={sendEmail} className='flex flex-col max-w-[600px] w-full'>
           <div className='pb-8'>
             <p className='text-4xl font-bold  text-white'>Support</p>
-            <p className='text-white py-4'>If you have any questions sumbit the form below:</p>
+            <p className='text-white py-4'>If you have any questions submit the form below:</p>
           </div>
           <input className=' bg-transparent p-2 border-2 rounded-xl border-[#00df9a] text-white' type="text" placeholder='Name *' name='from_name' />
           <input className='my-4 p-2 border-2 border-[#00df9a] rounded-xl bg-transparent text-white' type="email" placeholder='Email *' name='email_id' />
