e90eb451253338d31e1b5211d2714ecef6bb0cac
Arian Beke <131852639+arianBeke@users.noreply.github.com>
2024-04-17 15:28:58 +0200

    Added responsiveness

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,23 +1,22 @@
 import Link from "next/link";
 
-
 const navigation = [
-  { name: 'Support', href: './pages/support/' },
+  { name: 'Support', href: '/pages/support/' }
 ];
 
 export default function Navbar() {
   return (
-    <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
-      <Link href="/" className="font-bold text-white text-3xl">
+    <nav className="w-full relative flex flex-col items-center justify-between lg:flex-row lg:items-center lg:justify-between max-w-2xl mx-auto px-4 py-5">
+      <Link href="/" className="font-bold text-white text-2xl lg:text-3xl">
         Beke<span className="text-[#00df9a]">Gallery</span>
       </Link>
-      <div className="hidden lg:flex lg:gap-x-12">
-          {navigation.map((item) => (
-            <a key={item.name} href={item.href} className="text-l font-semibold leading-6 hover:text-[#00df9a] text-white">
-              {item.name}
-            </a>
-          ))}
-        </div>
+      <div className="lg:flex mt-5 md:mt-0 lg:gap-x-12">
+        {navigation.map((item) => (
+          <a key={item.name} href={item.href} className="text-l lg:text-base font-semibold leading-6 hover:text-[#00df9a] text-white">
+            {item.name}
+          </a>
+        ))}
+      </div>
     </nav>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ async function getData() {
   const query = `
   *[_type == 'blog'] | order(_createdAt desc) {
     title,
-      smallDescription,
-      "currentSlug": slug.current,
-      titleImage
+    smallDescription,
+    "currentSlug": slug.current,
+    titleImage
   }`;
 
   const data = await client.fetch(query);
@@ -25,10 +25,10 @@ export default async function Home() {
   console.log(data);
 
   return (
-    <div className="flex h-auto  flex-col items-center mt-5">
-      <div className="grid grid-cols-1 md:grid-cols-2 mt-5 w-[800px] gap-10">
+    <div className="flex h-auto flex-col items-center mt-10">
+      <div className="grid grid-cols-1 gap-10 md:grid-cols-2 md:gap-10 max-w-3xl">
         {data.map((post, idx) => (
-          <div className="ring-2  ring-white" key={idx}>
+          <div className="ring-2 ring-white" key={idx}>
             <div className="ring-2 ring-white">
               <Image
                 src={urlFor(post.titleImage).url()}
@@ -39,22 +39,22 @@ export default async function Home() {
               />
             </div>
 
-            <div className="mt-5 p-7">
+            <div className="mt-5 p-4">
               <h3 className="text-lg line-clamp-2 text-white font-bold">{post.title}</h3>
-              <p className="line-clamp-3 pb-1 text-sm mt-5 text-[#00df9a]">
+              <p className="line-clamp-3 pb-1 text-sm mt-2 text-[#00df9a]">
                 <span className="text-white">Published by: </span>
                 {post.smallDescription}
               </p>
               <Link href={`/blog/${post.currentSlug}`}>
-                <button className="rounded-md w-full bg-[#00df9a] mt-5 hover:text-[#00df9a] text-black hover:bg-gray-700 transition-colors duration-300  px-3.5 py-2.5 text-sm font-semibold shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">
-                View Image
+                <button className="rounded-md w-full bg-[#00df9a] mt-3 hover:text-[#00df9a] text-black hover:bg-gray-700 transition-colors duration-300 px-3 py-2 text-sm font-semibold shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white">
+                  View Image
                 </button>
               </Link>
             </div>
           </div>
         ))}
       </div>
-      <div className="h-40"></div>
+      <div className="h-20"></div>
     </div>
   );
 }
